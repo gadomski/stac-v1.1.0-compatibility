a3b5b5cff21750e029d44c432bfc03ae0717d001
Pete Gadomski <pete.gadomski@gmail.com>
2024-09-25 17:26:01 -0600

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# STAC v1.1.0 compatibility
+
+[STAC v1.1.0](https://github.com/radiantearth/stac-spec/releases/tag/v1.1.0) was released September 2024.
+This repository tracks the software ecosystem's update of the new release.
+
+**NOTE**: As of 2024-09-25 this repo is under construction as new repositories are added.
+If you'd like to see a repository added, please open [an issue](https://github.com/gadomski/stac-v1.1.0-compatability/issues).
+
+## Repository
+
+| Name | Version | Description | Read | Write |
+| -- | -- | -- | -- | -- |
+| [pystac](https://github.com/stac-utils/pystac) | 1.10.1 | Python library for working with any SpatioTemporal Asset Catalog (STAC) | ✅ | ❌ |
+
+## Usage
+
+```shell
+pip install -r requirements.txt
+python render.py
+```
---
+++ render.py
@@ -0,0 +1,63 @@
+import dataclasses
+import importlib.metadata
+from dataclasses import dataclass
+from pathlib import Path
+
+import jinja2
+import pystac.version
+from jinja2 import Environment, PackageLoader
+
+root = Path(__file__).parent
+examples = root / "examples"
+item = examples / "simple-item.json"
+
+
+class Result:
+    pass
+
+
+@dataclass
+class Repository:
+    name: str
+    url: str
+    description: str
+    version: str
+    read: bool
+    write: bool
+
+
+def pystac() -> Repository:
+    import pystac
+
+    pystac_item = pystac.read_file(item)
+    read = True
+    d = pystac_item.to_dict()
+    write = d["stac_version"] == "1.1.0"
+
+    return Repository(
+        "pystac",
+        "https://github.com/stac-utils/pystac",
+        "Python library for working with any SpatioTemporal Asset Catalog (STAC)",
+        version=importlib.metadata.version("pystac"),
+        read=read,
+        write=write,
+    )
+
+
+repositories = [dataclasses.asdict(d) for d in [pystac()]]
+
+
+def emoji(value: bool) -> str:
+    if value:
+        return "✅"
+    else:
+        return "❌"
+
+
+environment = Environment(
+    loader=PackageLoader("render"), autoescape=jinja2.select_autoescape()
+)
+environment.filters["emoji"] = emoji
+template = environment.get_template("README.md.jinja2")
+with open(root / "README.md", "w") as f:
+    f.write(template.render({"repositories": repositories}))
